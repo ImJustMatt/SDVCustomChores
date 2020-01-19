7922799e2f82fbe7482fd18cf379379709ce10ac
Matthew <justmatt@outlook.com>
2020-01-19 15:40:51 -0600

    Added additional params to API for more control

--- CustomChores/Framework/ICustomChoresAPI.cs
+++ CustomChores/Framework/ICustomChoresAPI.cs
@@ -2,7 +2,9 @@
 {
     public interface ICustomChoresApi
     {
-        /// <summary>Add a chore.</summary>
-        void AddCustomChore(ICustomChore chore);
+        /// <summary>Add a new custom chore to the game.</summary>
+        /// <param name="name">The name of the chore for config/commands.</param>
+        /// <param name="chore">A chore which performs one or more in-game tasks.</param>
+        void AddCustomChore(string name, ICustomChore chore);
     }
 }
