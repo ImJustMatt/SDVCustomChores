20f73a8dfc1336b00066f2f3d5df9a34a72e1884
Matthew <justmatt@outlook.com>
2020-01-26 15:38:28 -0600

    Moved solution to new folder

--- CustomChoresMod/Framework/ICustomChore.cs
+++ CustomChoresMod/Framework/ICustomChore.cs
@@ -8,15 +8,15 @@ namespace LeFauxMatt.CustomChores.Framework
         /*********
         ** Public methods
         *********/
-        /// <summary>Returns true if chore can be performed in current days conditions.</summary>
+        /// <summary>Returns true if chore can be performed based on the current days conditions.</summary>
         /// <param name="spouse">A reference to the farmer's spouse.</param>
         bool CanDoIt(NPC spouse);
 
-        /// <summary>Performs the chore.</summary>
+        /// <summary>Performs the chore and returns true/false on success or failure.</summary>
         /// <param name="spouse">A reference to the farmer's spouse.</param>
         bool DoIt(NPC spouse);
 
-        /// <summary>Performs the chore.</summary>
+        /// <summary>Returns the dialogue to be displayed on the day the chore is performed.</summary>
         /// <param name="spouse">A reference to the farmer's spouse.</param>
         Translation GetDialogue(NPC spouse);
     }
